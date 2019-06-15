ed5abd48eb6c1156b343c0a4e22761ec575671c4
David Kanfi <davidk1979@gmail.com>
2019-06-16 01:42:05 +0300

    my-todolist

---
+++ todo.js
@@ -0,0 +1,70 @@
+app.controller("TodoCtrl", function ($scope) {
+
+  $scope.taskList = [{
+      done: true,
+      task: 'Do nothing'
+    },
+    {
+      done: false,
+      task: 'Learn AngularJS'
+    },
+    {
+      done: false,
+      task: 'Build an app'
+    },
+    {
+      done: false,
+      task: 'Show some tasks'
+    },
+    {
+      done: false,
+      task: 'Add a task'
+    },
+    {
+      done: false,
+      task: 'Walk the dog'
+    }
+  ];
+
+  $scope.getTotal = function () {
+    return $scope.taskList.length;
+  };
+ 
+  //Function for adding task to the task list
+  $scope.addTask = function (task) {
+    //I'm pushing a new task to the task list
+    $scope.taskList.push({
+      done: false,
+      task: task
+    });
+    $scope.newTask = "";
+  };
+
+  $scope.clear = function() {
+    var oldList = $scope.taskList;
+    $scope.taskList = [];
+    angular.forEach(oldList, function(task) {
+        if (!task.done) $scope.taskList.push(task);
+    });
+};
+
+$scope.completed = function() {
+  var oldList = $scope.taskList;
+  $scope.taskList = [];
+  angular.forEach(oldList, function(task) {
+      if (task.done) $scope.taskList.push(task);
+  });
+};
+
+
+$scope.onSelect = function(task) {
+  if ($scope.selected === task) {
+    $scope.selected = null;
+  } else {
+    $scope.selected = task;
+
+  }
+}
+
+
+});
